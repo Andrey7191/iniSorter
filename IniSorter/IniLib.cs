adcfa8b2037b868d5f743467f7e9c344cd9ba376
AdminTest <perevozkin.andrey@yandex.ru>
2020-11-21 01:32:59 +0500

    First commit of IniSorter

---
+++ IniSorter/IniLib.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace IniSorter
+{
+    //Функциональный блок программы
+    static class IniLib
+    {
+        //Главный метод, отвечающий за сортировку всех ini-файлов в директории
+        //Принимает в качестве параметра путь к директории, в которой необходимо провести сортировку
+        //Возвращает код глобальной ошибки (например, если не удалось вообще найти директорию), количество отсортированных файлов и количество файлов с ошибками 
+        public static (string globalErrorType, int doneCount, int errorsCount) SortIniFilesInDir(string input_path)
+        {
+            string[] ini_files;
+            try
+            {
+                ini_files = Directory.GetFiles(input_path);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return ("DirectoryNotFound", 0, 0);
+            }
+            catch (Exception)
+            {
+                return ("DirectoryError", 0, 0);
+            }
+
+            Regex reg_ini_file = new Regex(@"^.+(\.ini){1}$");
+            Regex reg_ini_file_sorted = new Regex(@"^.+(_Sorted\.ini){1}$");
+
+            int ini_files_done = 0;
+            int ini_files_errors = 0;
+            for (int i = 0; i < ini_files.Length; i++)
+                if (reg_ini_file.IsMatch(ini_files[i]) && !reg_ini_file_sorted.IsMatch(ini_files[i]))
+                {
+                    string error = SortIniFile(ini_files[i], ini_files[i].Insert(ini_files[i].Length - 4, "_Sorted"));
+                    if (error == "")
+                        ini_files_done++;
+                    else
+                        ini_files_errors++;
+                }
+
+            if (ini_files_done == 0 && ini_files_errors== 0)
+            {
+                return ("FilesNotFound", 0, 0);
+            }
+
+            return ("", ini_files_done, ini_files_errors);
+        }
+
+
+        //Главный метод, отвечающий за сортировку одного ini-файла
+        //Принимает на вход путь (включая название) к сортируемому файлу и путь (включая название), по которому нужно сохранить файл
+        //Возвращает код ошибки или пустую строку, если сортировка прошла успешно
+        public static string SortIniFile(string input_file_path, string output_file_path)
+        {
+            string[] file_content;
+            try
+            {
+                file_content = File.ReadAllLines(input_file_path);
+            }
+            catch (FileNotFoundException)
+            {
+                return "FileNotFound";
+            }
+            catch (ArgumentException)
+            {
+                return "EmptyPathName";
+            }
+            catch (Exception)
+            {
+                return "UnknownReadError";
+            }
+
+            List<IniSection> ini_content = SeparateIniFileContent(file_content);
+            if (ini_content.Count == 0)
+                return "IncorrectSyntax";
+
+            ini_content = SortIniSections(ini_content);
+
+            string ini_file_text = ConvertIniSectionsToString(ini_content);
+            try
+            {
+                File.WriteAllText(output_file_path, ini_file_text);
+            }
+            catch (FileNotFoundException)
+            {
+                return "FileNotFound";
+            }
+
+            return "";
+        }
+
+
+        //Два вспомогательных класса для хранения извлечённой из ini-файла информации с разбиением по семантике
+        private class IniSection
+        {
+            public string section_name;
+            public List<IniSectionInner> section_inners = new List<IniSectionInner>();
+        }
+
+
+        private class IniSectionInner
+        {
+            public string parameter = "";
+            public string value = "";
+        }
+
+
+
+        //Метод, разделяющий информацию, считанную из ini-файла, и представляющий её с помощью вспомогательных классов
+        private static List<IniSection> SeparateIniFileContent(string[] file_content)
+        {
+            List<IniSection> ini_content = new List<IniSection>();
+
+            for (int i = 0; i < file_content.Length; i++)
+            {
+                if (file_content[i].IndexOf(";") >= 0)
+                    file_content[i] = file_content[i].Split(";")[0];
+                if (file_content[i].IndexOf("#") >= 0)
+                    file_content[i] = file_content[i].Split("#")[0];
+
+                string line_type = GetIniLineType(file_content[i]);
+
+                if (line_type == "EmptyLine")
+                {
+                    continue;
+                }
+                else if (line_type == "SectionName")
+                {
+                    IniSection ini_section = new IniSection();
+
+                    ini_section.section_name = file_content[i].Trim();
+                    ini_content.Add(ini_section);
+                }
+                else if (line_type == "SectionInner")
+                {
+                    IniSectionInner section_inner = new IniSectionInner();
+                    string[] split_line = file_content[i].Split("=");
+
+                    section_inner.parameter = split_line[0].Trim();
+                    section_inner.value = split_line[1].Trim();
+
+                    if (ini_content.Count != 0)
+                        ini_content[ini_content.Count - 1].section_inners.Add(section_inner);
+                    else
+                        return ini_content;
+                }
+                else
+                {
+                    ini_content.Clear();
+                    return ini_content;
+                }
+            }
+
+            return ini_content;
+        }
+
+
+        //Метод, распознающий в переданной строке тот или иной элемент синтаксиса ini-файла (заголовок секции, содержание секции, пустая строка)
+        private static string GetIniLineType(string ini_line)
+        {
+            Regex reg_section_name = new Regex(@"^\s*[\[]{1}\s*[a-zA-Z0-9\._\-\\\/%:\, ]+\s*[\]]{1}\s*$");
+            //Менее строгий вариант
+            Regex reg_section_inner = new Regex(@"^\s*[a-zA-Z0-9\._\-\\\/%:\, ]+\s*={1}[^=\[\]]*$");
+            //Более строгий вариант
+            //Regex reg_section_inner = new Regex(@"^\s*[a-zA-Z0-9\._\-\\\/%:\, ]+\s*={1}[a-zA-Z0-9\._\-\\\/%:\, +""'']*$");
+            
+            Regex reg_empty = new Regex(@"^\s*$");
+
+            string ini_line_type;
+
+            if (reg_section_name.IsMatch(ini_line))
+                ini_line_type = "SectionName";
+            else if (reg_section_inner.IsMatch(ini_line))
+                ini_line_type = "SectionInner";
+            else if (reg_empty.IsMatch(ini_line))
+                ini_line_type = "EmptyLine";
+            else
+                ini_line_type = "UnknownType";
+
+            return ini_line_type;
+        }
+
+
+        //Метод, непосредственно ответственный за сортировку содержимого ini-файла
+        private static List<IniSection> SortIniSections(List<IniSection> ini_sections)
+        {
+            IniSection temp_section;
+            for (int i = 0; i < ini_sections.Count; i++)
+            {
+                for (int j = i + 1; j < ini_sections.Count; j++)
+                {
+                    if (String.Compare(ini_sections[i].section_name, ini_sections[j].section_name) == 1)
+                    {
+                        temp_section = ini_sections[i];
+                        ini_sections[i] = ini_sections[j];
+                        ini_sections[j] = temp_section;
+                    }
+                }
+            }
+
+            IniSectionInner temp_section_inner;
+            for (int n = 0; n < ini_sections.Count; n++)
+            {
+                for (int i = 0; i < ini_sections[n].section_inners.Count; i++)
+                {
+                    for (int j = i + 1; j < ini_sections[n].section_inners.Count; j++)
+                    {
+                        if (String.Compare(ini_sections[n].section_inners[i].parameter, ini_sections[n].section_inners[j].parameter) == 1)
+                        {
+                            temp_section_inner = ini_sections[n].section_inners[i];
+                            ini_sections[n].section_inners[i] = ini_sections[n].section_inners[j];
+                            ini_sections[n].section_inners[j] = temp_section_inner;
+                        }
+                    }
+                }
+            }
+
+            return ini_sections;
+        }
+
+
+        //Метод, ответственный за "упаковку" содержания ini-файла в строку для последующей записи в ini-файл
+        private static string ConvertIniSectionsToString(List<IniSection> ini_content)
+        {
+            string file_text = "";
+            for (int i = 0; i < ini_content.Count; i++)
+            {
+                string section_name_line = ini_content[i].section_name + "\n";
+                for (int j = 0; j < ini_content[i].section_inners.Count; j++)
+                {
+                    string secrion_inner_line = ini_content[i].section_inners[j].parameter + "=" + ini_content[i].section_inners[j].value + "\n";
+                    section_name_line = section_name_line + secrion_inner_line;
+
+                }
+                file_text = file_text + "\n" + section_name_line;
+            }
+            return file_text;
+        }
+    }
+}
---
+++ IniSorter/IniSorterInterface.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+//IniSorter - программа для сортировки содержания ini-файлов.
+//Перевозкин А. А., 2020 г.
+
+namespace IniSorter
+{
+    //Интерфейс программы
+    class IniSorterInterface
+    {
+        //Главный метод интерфейса программы. Отвечает за обработку команд пользователя, проверку правильности ввода.
+        //Доступно 4 команды: сортировка одного ini-файла, сортировка всех ini-файлов в директории, вывод подсказки, выход из программы.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в программу IniSorter! \nПрограмма предназначена для сортировки содержимого Ваших ini-файлов. \nВсе отсортированный файла сохраняются в ту же директорию с добавление '_Sorted' к названию.");
+            string help_text = "Введите: \n'sortfile путь-к-файлу' - для сортировки 1-го файла, \n'sortdir путь-к-директории' - для сортировки всех ini-файлов в директории, \n'help' - для вывода этой подсказки, \n'exit' - для выхода.";
+            Console.WriteLine(help_text);
+            while (true)
+            {
+                Console.Write("$ ");
+                string command = Console.ReadLine();
+
+                string[] splited_direction;
+                string input_file_path = "";
+                string input_directory = "";
+                Regex reg_ini_file = new Regex(@"^.+(\.ini){1}$");
+
+                string command_type = GetСommandType(command);
+
+                if (command_type=="exit")
+                {
+                    break;
+                }
+                else if (command_type == "help")
+                {
+                    Console.WriteLine(help_text);
+                }
+                else if (command_type == "sortfile")
+                {
+                    splited_direction = command.Trim().Split("sortfile");
+                    splited_direction[1] = splited_direction[1].Trim();
+                    if (splited_direction[1]=="")
+                        Console.WriteLine("Название файла не может быть пустым! Попробуйте снова!");
+                    else if (!reg_ini_file.IsMatch(splited_direction[1]))
+                        Console.WriteLine("Файл должен заканчиваться на '.ini'. Попробуйте снова!");
+                    else
+                        input_file_path = splited_direction[1];
+                }
+                else if (command_type == "sortdir")
+                {
+                    splited_direction = command.Trim().Split("sortdir");
+                    splited_direction[1] = splited_direction[1].Trim();
+                    if (splited_direction[1] == "")
+                        Console.WriteLine("Путь не может быть пустым! Попробуйте снова!");
+                    else
+                        input_directory = splited_direction[1];
+                }
+                else
+                {
+                    Console.WriteLine("Введена недопустимая команда! Введите'help' для вывода доступных команд.");
+                }
+
+                if (input_file_path != "")
+                {
+                    string output_file_path = input_file_path.Insert(input_file_path.Length - 4, "_Sorted");
+                    string error = IniLib.SortIniFile(input_file_path, output_file_path);
+                    HandleSortIniFileErrors(error);
+                }
+                else if (input_directory != "")
+                {
+                    (string globalErrorType, int doneCount, int errorsCount) result = IniLib.SortIniFilesInDir(input_directory);
+                    HandleSortIniFilesInDirErrors(result);
+                }
+            }
+
+
+        }
+
+        //Метод, отвечающий за определение типа команды пользователя
+        //Возвращает: "exit", "help", "sortfile", "sortdir" или "UnknownType"
+        private static string GetСommandType(string command)
+        {
+            Regex reg_exit = new Regex(@"^\s*(exit){1}\s*$");
+            Regex reg_help = new Regex(@"^\s*(help){1}\s*$");
+            Regex reg_sort_file = new Regex(@"^\s*(sortfile){1}.*$");
+            Regex reg_sort_dir = new Regex(@"^\s*(sortdir){1}.*$");
+
+            string command_type;
+
+            if (reg_exit.IsMatch(command))
+                command_type = "exit";
+            else if (reg_help.IsMatch(command))
+                command_type = "help";
+            else if (reg_sort_file.IsMatch(command))
+                command_type = "sortfile";
+            else if (reg_sort_dir.IsMatch(command))
+                command_type = "sortdir";
+            else
+                command_type = "UnknownType";
+
+            return command_type;
+        }
+
+
+        //Метод, отвечающий за обработку результатов функции сортировки одного файла. Результаты работы выводятся на экран.
+        private static void HandleSortIniFileErrors(string error)
+        {
+            switch (error)
+            {
+                case "":
+                    Console.WriteLine("Файл отсортирован и сохранён. Можете отсортировать ещё один файл!");
+                    break;
+                case "FileNotFound":
+                    Console.WriteLine("Файл не найден. Попробуйте снова!");
+                    break;
+                case "IncorrectSyntax":
+                    Console.WriteLine("Файл не соответствует синтаксису ini, не может быть отсортирован. Попробуйте снова!");
+                    break;
+                case "UnknownReadError":
+                    Console.WriteLine("Не удалось прочитать информацию из файла. Попробуйте снова!");
+                    break;
+                case "EmptyPathName":
+                    Console.WriteLine("Был указан пустой путь! Попробуйте снова!");
+                    break;
+                default:
+                    Console.WriteLine("Возникла непредвиденная ошибка. Попробуйте снова!");
+                    break;
+            }
+        }
+
+
+        //Метод, отвечающий за обработку результатов функции сортировки всех файлов в директории. Результаты работы выводятся на экран.
+        private static void HandleSortIniFilesInDirErrors((string globalErrorType, int doneCount, int errorsCount) result)
+        {
+            switch (result.globalErrorType)
+            {
+                case "":
+                    Console.WriteLine("Отсортировано файлов: " + result.doneCount + ", ошибок: " + result.errorsCount);
+                    break;
+                case "FilesNotFound":
+                    Console.WriteLine("Ini-файлы в указанной директории не найдены. Попробуйте снова!");
+                    break;
+                case "DirectoryNotFound":
+                    Console.WriteLine("Директория не найдена. Попробуйте снова!");
+                    break; 
+                case "DirectoryError":
+                    Console.WriteLine("Не удалось прочитать файлы из директории. Попробуйте снова!");
+                    break;
+                default:
+                    Console.WriteLine("Возникла непредвиденная ошибка. Попробуйте снова!");
+                    break;
+            }
+        }
+
+    }
+}
