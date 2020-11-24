cde6740e2c6f77b29c6a3dd60f09ecbefe0dd9c4
AdminTest <perevozkin.andrey@yandex.ru>
2020-11-24 06:09:27 +0500

    Refactoring. Сustom classes replaced with SortedDictionary

--- IniSorter/IniLib.cs
+++ IniSorter/IniLib.cs
@@ -6,17 +6,17 @@ using System.Text.RegularExpressions;
 namespace IniSorter
 {
     //Функциональный блок программы
-    static class IniLib
+    public static class IniLib
     {
         //Главный метод, отвечающий за сортировку всех ini-файлов в директории
         //Принимает в качестве параметра путь к директории, в которой необходимо провести сортировку
         //Возвращает код глобальной ошибки (например, если не удалось вообще найти директорию), количество отсортированных файлов и количество файлов с ошибками 
-        public static (string globalErrorType, int doneCount, int errorsCount) SortIniFilesInDir(string input_path)
+        public static (string globalErrorType, int doneCount, int errorsCount) SortIniFilesInDir(string inputPath)
         {
-            string[] ini_files;
+            string[] iniFiles;
             try
             {
-                ini_files = Directory.GetFiles(input_path);
+                iniFiles = Directory.GetFiles(inputPath);
             }
             catch (DirectoryNotFoundException)
             {
@@ -27,39 +27,41 @@ namespace IniSorter
                 return ("DirectoryError", 0, 0);
             }
 
-            Regex reg_ini_file = new Regex(@"^.+(\.ini){1}$");
-            Regex reg_ini_file_sorted = new Regex(@"^.+(_Sorted\.ini){1}$");
+            Regex regIniFile = new Regex(@"^.+(\.ini){1}$");
+            Regex regIniFileSorted = new Regex(@"^.+(_Sorted\.ini){1}$");
 
-            int ini_files_done = 0;
-            int ini_files_errors = 0;
-            for (int i = 0; i < ini_files.Length; i++)
-                if (reg_ini_file.IsMatch(ini_files[i]) && !reg_ini_file_sorted.IsMatch(ini_files[i]))
+            int iniFilesDone = 0;
+            int iniFilesErrors = 0;
+
+            foreach (string iniFileName in iniFiles)
+                if (regIniFile.IsMatch(iniFileName) && !regIniFileSorted.IsMatch(iniFileName))
                 {
-                    string error = SortIniFile(ini_files[i], ini_files[i].Insert(ini_files[i].Length - 4, "_Sorted"));
+                    string error = SortIniFile(iniFileName, iniFileName.Insert(iniFileName.Length - 4, "_Sorted"));
                     if (error == "")
-                        ini_files_done++;
+                        iniFilesDone++;
                     else
-                        ini_files_errors++;
+                        iniFilesErrors++;
                 }
-
-            if (ini_files_done == 0 && ini_files_errors== 0)
+                
+            if (iniFilesDone == 0 && iniFilesErrors== 0)
             {
                 return ("FilesNotFound", 0, 0);
             }
 
-            return ("", ini_files_done, ini_files_errors);
+            return ("", iniFilesDone, iniFilesErrors);
         }
 
 
         //Главный метод, отвечающий за сортировку одного ini-файла
         //Принимает на вход путь (включая название) к сортируемому файлу и путь (включая название), по которому нужно сохранить файл
         //Возвращает код ошибки или пустую строку, если сортировка прошла успешно
-        public static string SortIniFile(string input_file_path, string output_file_path)
+        public static string SortIniFile(string inputFilePath, string outputFilePath)
         {
-            string[] file_content;
+            //Чтение информации из файла
+            string[] fileContent;
             try
             {
-                file_content = File.ReadAllLines(input_file_path);
+                fileContent = File.ReadAllLines(inputFilePath);
             }
             catch (FileNotFoundException)
             {
@@ -74,16 +76,24 @@ namespace IniSorter
                 return "UnknownReadError";
             }
 
-            List<IniSection> ini_content = SeparateIniFileContent(file_content);
-            if (ini_content.Count == 0)
-                return "IncorrectSyntax";
-
-            ini_content = SortIniSections(ini_content);
-
-            string ini_file_text = ConvertIniSectionsToString(ini_content);
+            //Перенос информации, полученной из файла, в сортированный словарь формата "название-секции : содержание-секции(параметр : значение)"
+            SortedDictionary<string, SortedDictionary<string, string>> iniContent = new SortedDictionary<string, SortedDictionary<string, string>>();
             try
             {
-                File.WriteAllText(output_file_path, ini_file_text);
+                iniContent = SeparateIniFileContent(fileContent);
+            }
+            catch (Exception)
+            {
+                return "IncorrectSyntax";
+            }
+            if (iniContent.Count == 0)
+                return "IncorrectSyntax";
+
+            //Перенос информации из сортированных словарей в выходной файл
+            string iniFileText = ConvertIniContentToString(iniContent);
+            try
+            {
+                File.WriteAllText(outputFilePath, iniFileText);
             }
             catch (FileNotFoundException)
             {
@@ -94,151 +104,86 @@ namespace IniSorter
         }
 
 
-        //Два вспомогательных класса для хранения извлечённой из ini-файла информации с разбиением по семантике
-        private class IniSection
+        //Метод, разделяющий информацию, считанную из ini-файла, и представляющий её в виде сортированных словарей
+        //Возвращает словарь формата "название-секции : содержание-секции(параметр : значение)"
+        private static SortedDictionary<string, SortedDictionary<string, string>> SeparateIniFileContent(string[] fileContent)
         {
-            public string section_name;
-            public List<IniSectionInner> section_inners = new List<IniSectionInner>();
-        }
+            SortedDictionary<string, SortedDictionary<string, string>> iniContent = new SortedDictionary<string, SortedDictionary<string, string>>();
+            string sectionName = "";
 
-
-        private class IniSectionInner
-        {
-            public string parameter = "";
-            public string value = "";
-        }
-
-
-
-        //Метод, разделяющий информацию, считанную из ini-файла, и представляющий её с помощью вспомогательных классов
-        private static List<IniSection> SeparateIniFileContent(string[] file_content)
-        {
-            List<IniSection> ini_content = new List<IniSection>();
-
-            for (int i = 0; i < file_content.Length; i++)
+            for (int i = 0; i < fileContent.Length; i++)
             {
-                if (file_content[i].IndexOf(";") >= 0)
-                    file_content[i] = file_content[i].Split(";")[0];
-                if (file_content[i].IndexOf("#") >= 0)
-                    file_content[i] = file_content[i].Split("#")[0];
+                if (fileContent[i].IndexOf(";") >= 0)
+                    fileContent[i] = fileContent[i].Split(";")[0];
+                if (fileContent[i].IndexOf("#") >= 0)
+                    fileContent[i] = fileContent[i].Split("#")[0];
 
-                string line_type = GetIniLineType(file_content[i]);
-
-                if (line_type == "EmptyLine")
+                string lineType = GetIniLineType(fileContent[i]);
+                switch (lineType)
                 {
-                    continue;
-                }
-                else if (line_type == "SectionName")
-                {
-                    IniSection ini_section = new IniSection();
-
-                    ini_section.section_name = file_content[i].Trim();
-                    ini_content.Add(ini_section);
-                }
-                else if (line_type == "SectionInner")
-                {
-                    IniSectionInner section_inner = new IniSectionInner();
-                    string[] split_line = file_content[i].Split("=");
-
-                    section_inner.parameter = split_line[0].Trim();
-                    section_inner.value = split_line[1].Trim();
-
-                    if (ini_content.Count != 0)
-                        ini_content[ini_content.Count - 1].section_inners.Add(section_inner);
-                    else
-                        return ini_content;
-                }
-                else
-                {
-                    ini_content.Clear();
-                    return ini_content;
+                    case "EmptyLine":
+                        continue;
+                    case "SectionName":
+                        sectionName = fileContent[i].Trim()[1..^1].Trim();
+                        if (!iniContent.ContainsKey(sectionName))
+                            iniContent.Add(sectionName, new SortedDictionary<string, string>());
+                        break;
+                    case "SectionInner":
+                        string[] splitedLine = fileContent[i].Split("=");
+                        string parameter = splitedLine[0].Trim();
+                        string value = splitedLine[1].Trim();
+                        if (sectionName != "")
+                            if (!iniContent.ContainsKey(parameter))
+                                iniContent[sectionName].Add(parameter, value);
+                            else
+                                iniContent[sectionName][parameter] = value;
+                        else
+                            return iniContent;
+                        break;
+                    default:
+                        iniContent.Clear();
+                        return iniContent;
                 }
             }
 
-            return ini_content;
+            return iniContent;
         }
 
 
         //Метод, распознающий в переданной строке тот или иной элемент синтаксиса ini-файла (заголовок секции, содержание секции, пустая строка)
-        private static string GetIniLineType(string ini_line)
+        private static string GetIniLineType(string iniLine)
         {
-            Regex reg_section_name = new Regex(@"^\s*[\[]{1}\s*[a-zA-Z0-9\._\-\\\/%:\, ]+\s*[\]]{1}\s*$");
-            //Менее строгий вариант
-            Regex reg_section_inner = new Regex(@"^\s*[a-zA-Z0-9\._\-\\\/%:\, ]+\s*={1}[^=\[\]]*$");
-            //Более строгий вариант
-            //Regex reg_section_inner = new Regex(@"^\s*[a-zA-Z0-9\._\-\\\/%:\, ]+\s*={1}[a-zA-Z0-9\._\-\\\/%:\, +""'']*$");
-            
-            Regex reg_empty = new Regex(@"^\s*$");
+            Regex regSectionName = new Regex(@"^\s*[\[]{1}\s*[a-zA-Z0-9\._\-\\\/%:'""'\*<>]+[^=\[\]]*[\]]{1}\s*$");
+            Regex regSectionInner = new Regex(@"^\s*[a-zA-Z0-9\._\-\\\/%:'""'\*<>]+[^=\[\]]*={1}[^=\[\]]*$");
+            Regex regEmpty = new Regex(@"^\s*$");
 
-            string ini_line_type;
+            string iniLineType;
 
-            if (reg_section_name.IsMatch(ini_line))
-                ini_line_type = "SectionName";
-            else if (reg_section_inner.IsMatch(ini_line))
-                ini_line_type = "SectionInner";
-            else if (reg_empty.IsMatch(ini_line))
-                ini_line_type = "EmptyLine";
+            if (regSectionName.IsMatch(iniLine))
+                iniLineType = "SectionName";
+            else if (regSectionInner.IsMatch(iniLine))
+                iniLineType = "SectionInner";
+            else if (regEmpty.IsMatch(iniLine))
+                iniLineType = "EmptyLine";
             else
-                ini_line_type = "UnknownType";
+                iniLineType = "UnknownType";
 
-            return ini_line_type;
-        }
-
-
-        //Метод, непосредственно ответственный за сортировку содержимого ini-файла
-        private static List<IniSection> SortIniSections(List<IniSection> ini_sections)
-        {
-            IniSection temp_section;
-            for (int i = 0; i < ini_sections.Count; i++)
-            {
-                for (int j = i + 1; j < ini_sections.Count; j++)
-                {
-                    if (String.Compare(ini_sections[i].section_name, ini_sections[j].section_name) == 1)
-                    {
-                        temp_section = ini_sections[i];
-                        ini_sections[i] = ini_sections[j];
-                        ini_sections[j] = temp_section;
-                    }
-                }
-            }
-
-            IniSectionInner temp_section_inner;
-            for (int n = 0; n < ini_sections.Count; n++)
-            {
-                for (int i = 0; i < ini_sections[n].section_inners.Count; i++)
-                {
-                    for (int j = i + 1; j < ini_sections[n].section_inners.Count; j++)
-                    {
-                        if (String.Compare(ini_sections[n].section_inners[i].parameter, ini_sections[n].section_inners[j].parameter) == 1)
-                        {
-                            temp_section_inner = ini_sections[n].section_inners[i];
-                            ini_sections[n].section_inners[i] = ini_sections[n].section_inners[j];
-                            ini_sections[n].section_inners[j] = temp_section_inner;
-                        }
-                    }
-                }
-            }
-
-            return ini_sections;
+            return iniLineType;
         }
 
 
         //Метод, ответственный за "упаковку" содержания ini-файла в строку для последующей записи в ini-файл
-        private static string ConvertIniSectionsToString(List<IniSection> ini_content)
+        private static string ConvertIniContentToString(SortedDictionary<string, SortedDictionary<string, string>> iniContent)
         {
-            string file_text = "";
-            for (int i = 0; i < ini_content.Count; i++)
+            string fileText = "";
+            foreach (KeyValuePair<string, SortedDictionary<string, string>> section in iniContent)
             {
-                string section_name_line = ini_content[i].section_name + "\n";
-                for (int j = 0; j < ini_content[i].section_inners.Count; j++)
-                {
-                    string secrion_inner_line = ini_content[i].section_inners[j].parameter + "=" + ini_content[i].section_inners[j].value + "\n";
-                    section_name_line = section_name_line + secrion_inner_line;
-
-                }
-                file_text = file_text + "\n" + section_name_line;
+                fileText = fileText + "\n[" + section.Key + "]\n";
+                foreach (KeyValuePair<string, string> sectionInner in iniContent[section.Key])
+                    fileText = fileText + sectionInner.Key + "=" + sectionInner.Value + "\n";
             }
-            return file_text;
+            return fileText;
         }
+
     }
 }
--- IniSorter/IniSorterInterface.cs
+++ IniSorter/IniSorterInterface.cs
@@ -14,92 +14,86 @@ namespace IniSorter
         static void Main(string[] args)
         {
             Console.WriteLine("Добро пожаловать в программу IniSorter! \nПрограмма предназначена для сортировки содержимого Ваших ini-файлов. \nВсе отсортированный файла сохраняются в ту же директорию с добавление '_Sorted' к названию.");
-            string help_text = "Введите: \n'sortfile путь-к-файлу' - для сортировки 1-го файла, \n'sortdir путь-к-директории' - для сортировки всех ini-файлов в директории, \n'help' - для вывода этой подсказки, \n'exit' - для выхода.";
-            Console.WriteLine(help_text);
+            string helpText = "Введите: \n'sortfile путь-к-файлу' - для сортировки 1-го файла, \n'sortdir путь-к-директории' - для сортировки всех ini-файлов в директории, \n'help' - для вывода этой подсказки, \n'exit' - для выхода.";
+            Console.WriteLine(helpText);
             while (true)
             {
                 Console.Write("$ ");
                 string command = Console.ReadLine();
+                string commandType = GetСommandType(command);
 
-                string[] splited_direction;
-                string input_file_path = "";
-                string input_directory = "";
-                Regex reg_ini_file = new Regex(@"^.+(\.ini){1}$");
-
-                string command_type = GetСommandType(command);
-
-                if (command_type=="exit")
-                {
+                string commandArgument;
+                string inputFilePath = "";
+                string inputDirectoryPath = "";
+                
+                if (commandType=="exit")
                     break;
-                }
-                else if (command_type == "help")
+
+                switch (commandType)
                 {
-                    Console.WriteLine(help_text);
-                }
-                else if (command_type == "sortfile")
-                {
-                    splited_direction = command.Trim().Split("sortfile");
-                    splited_direction[1] = splited_direction[1].Trim();
-                    if (splited_direction[1]=="")
-                        Console.WriteLine("Название файла не может быть пустым! Попробуйте снова!");
-                    else if (!reg_ini_file.IsMatch(splited_direction[1]))
-                        Console.WriteLine("Файл должен заканчиваться на '.ini'. Попробуйте снова!");
-                    else
-                        input_file_path = splited_direction[1];
-                }
-                else if (command_type == "sortdir")
-                {
-                    splited_direction = command.Trim().Split("sortdir");
-                    splited_direction[1] = splited_direction[1].Trim();
-                    if (splited_direction[1] == "")
-                        Console.WriteLine("Путь не может быть пустым! Попробуйте снова!");
-                    else
-                        input_directory = splited_direction[1];
-                }
-                else
-                {
-                    Console.WriteLine("Введена недопустимая команда! Введите'help' для вывода доступных команд.");
+                    case "help":
+                        Console.WriteLine(helpText);
+                        break;
+                    case "sortfile":
+                        Regex regIniFile = new Regex(@"^.+(\.ini){1}$");
+                        commandArgument = command.Trim().Split("sortfile")[1].Trim();
+                        if (commandArgument == "")
+                            Console.WriteLine("Название файла не может быть пустым! Попробуйте снова!");
+                        else if (!regIniFile.IsMatch(commandArgument))
+                            Console.WriteLine("Файл должен заканчиваться на '.ini'. Попробуйте снова!");
+                        else
+                            inputFilePath = commandArgument;
+                        break;
+                    case "sortdir":
+                        commandArgument = command.Trim().Split("sortdir")[1].Trim();
+                        if (commandArgument == "")
+                            Console.WriteLine("Путь не может быть пустым! Попробуйте снова!");
+                        else
+                            inputDirectoryPath = commandArgument;
+                        break;
+                    default:
+                        Console.WriteLine("Введена недопустимая команда! Введите'help' для вывода доступных команд.");
+                        break;
                 }
 
-                if (input_file_path != "")
+                if (inputFilePath != "")
                 {
-                    string output_file_path = input_file_path.Insert(input_file_path.Length - 4, "_Sorted");
-                    string error = IniLib.SortIniFile(input_file_path, output_file_path);
+                    string outputFilePath = inputFilePath.Insert(inputFilePath.Length - 4, "_Sorted");
+                    string error = IniLib.SortIniFile(inputFilePath, outputFilePath);
                     HandleSortIniFileErrors(error);
                 }
-                else if (input_directory != "")
+                else if (inputDirectoryPath != "")
                 {
-                    (string globalErrorType, int doneCount, int errorsCount) result = IniLib.SortIniFilesInDir(input_directory);
+                    (string globalErrorType, int doneCount, int errorsCount) result = IniLib.SortIniFilesInDir(inputDirectoryPath);
                     HandleSortIniFilesInDirErrors(result);
                 }
             }
-
-
         }
+
 
         //Метод, отвечающий за определение типа команды пользователя
         //Возвращает: "exit", "help", "sortfile", "sortdir" или "UnknownType"
         private static string GetСommandType(string command)
         {
-            Regex reg_exit = new Regex(@"^\s*(exit){1}\s*$");
-            Regex reg_help = new Regex(@"^\s*(help){1}\s*$");
-            Regex reg_sort_file = new Regex(@"^\s*(sortfile){1}.*$");
-            Regex reg_sort_dir = new Regex(@"^\s*(sortdir){1}.*$");
+            Regex regExit = new Regex(@"^\s*(exit){1}\s*$");
+            Regex regHelp = new Regex(@"^\s*(help){1}\s*$");
+            Regex regSortFile = new Regex(@"^\s*(sortfile){1}.*$");
+            Regex regSortDir = new Regex(@"^\s*(sortdir){1}.*$");
 
-            string command_type;
+            string commandType;
 
-            if (reg_exit.IsMatch(command))
-                command_type = "exit";
-            else if (reg_help.IsMatch(command))
-                command_type = "help";
-            else if (reg_sort_file.IsMatch(command))
-                command_type = "sortfile";
-            else if (reg_sort_dir.IsMatch(command))
-                command_type = "sortdir";
+            if (regExit.IsMatch(command))
+                commandType = "exit";
+            else if (regHelp.IsMatch(command))
+                commandType = "help";
+            else if (regSortFile.IsMatch(command))
+                commandType = "sortfile";
+            else if (regSortDir.IsMatch(command))
+                commandType = "sortdir";
             else
-                command_type = "UnknownType";
+                commandType = "UnknownType";
 
-            return command_type;
+            return commandType;
         }
 
 
